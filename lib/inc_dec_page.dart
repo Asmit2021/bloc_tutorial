68982bb1aa49a856c2571e56f2edd734f6006bf0
Asmit2021 <2140149@sliet.ac.in>
2024-01-26 17:46:28 +0530

    Learn

---
+++ lib/inc_dec_page.dart
@@ -0,0 +1,38 @@
+import 'package:bloc_tutorial/bloc/counter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+//import 'cubit/counter_cubit.dart';
+
+class IncDecPage extends StatelessWidget {
+  const IncDecPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //final counterCubit = BlocProvider.of<CounterCubit>(context);
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterIncremented());
+              //counterCubit.increment();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              //counterCubit.decrement();
+              counterBloc.add(CounterDecremented());
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
